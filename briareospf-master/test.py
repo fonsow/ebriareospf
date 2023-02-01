98fca29a24f4510c4a0614e2fe5de94e5da6acdc
Afonso Pinto <47603438+fonsow@users.noreply.github.com>
2023-02-01 00:41:17 +0000

    pfv

--- briareospf-master/test.py
+++ briareospf-master/test.py
@@ -3,6 +3,36 @@ import sys
 
 bpf_text = '''
 #include <linux/sched.h>
+#define MAX_SYSCALLS 1000000
+
+struct sys_event_t {
+    u32 id;
+    u64 pid_tgid;
+    u64 ns;
+};
+
+BPF_PERF_OUTPUT(events);
+BPF_HASH(start, u64, u64);
+
+TRACEPOINT_PROBE(raw_syscalls, sys_enter) {
+    u64 pid_tgid = bpf_get_current_pid_tgid();
+    u64 t = bpf_ktime_get_ns();
+    start.update(&pid_tgid, &t);
+    return 0;
+}
+
+TRACEPOINT_PROBE(raw_syscalls, sys_exit) {
+    u64 pid_tgid = bpf_get_current_pid_tgid();
+    u64 *start_ns = start.lookup(&pid_tgid);
+    if (!start_ns)
+        return 0;
+    struct sys_event_t event = {};
+    event.id = args->id;
+    event.pid_tgid = pid_tgid;
+    event.ns = bpf_ktime_get_ns() - *start_ns;
+    events.perf_submit(args, &event, sizeof(event));
+    return 0;
+}
 
 BPF_ARRAY(syscalls, u64, 100000);
 
